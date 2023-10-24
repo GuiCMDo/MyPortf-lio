3eded19d1c21e2278993c7f0c4ff22acf5c66ee0
GuiCMDo <guilhermecmdourado@outlook.com.br>
2023-10-24 02:25:37 -0300

    v1 - Inicializar dados do cliente

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,23 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         //Conta bancária
+
+        //Variáveis
+        Scanner sc = new Scanner(System.in);
+        String nome = "Guilherme Dourado";
+        String tipoDeConta = "Conta-Corrente";
+        double saldo = 5000.00;
+
+        //Inicializar dados do cliente;
+        System.out.println("****************************************************");
+        System.out.println("Dados Iniciais do cliente");
+        System.out.println(nome);
+        System.out.println(tipoDeConta);
+        System.out.println("R$ " + saldo);
+        System.out.println("****************************************************");
+        
+        //Criação do Menu de opções
     }
 }
