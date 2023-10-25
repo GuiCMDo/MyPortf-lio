f42987041ba5b9c7ac9019b51f0038f00690aec8
GuiCMDo <guilhermecmdourado@outlook.com.br>
2023-10-24 23:22:38 -0300

    Aplicação Finalizada - 90% (Necessário ajustar looping de menu de opções)

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,11 @@
-//import java.util.Scanner;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         //Conta bancária
 
         //Variáveis
-        //Scanner sc = new Scanner(System.in);
+        Scanner sc = new Scanner(System.in);
         String nome = "Guilherme Dourado";
         String tipoDeConta = "Conta-Corrente";
         double saldo = 5000.00;
@@ -19,5 +19,46 @@ public class Main {
         System.out.println("****************************************************");
         
         //Criação do Menu de opções
+        int opcao;
+
+        for (int i = 0;i < 4; i++) {
+            System.out.println("                          ");
+            System.out.println("Operações: ");
+            System.out.println("1 - Consultar saldos ");
+            System.out.println("2 - Receber valor ");
+            System.out.println("3 - Transferir valor ");
+            System.out.println("4 - Sair ");
+
+            System.out.println("Digite a opção desejada: ");
+            opcao = sc.nextInt();
+
+            if (opcao == 1) {
+                System.out.println("Seu saldo é de: R$ " + saldo);
+
+            } else if (opcao == 2) {
+                System.out.println("Informe o valor que está recebendo: ");
+                double credito = sc.nextDouble();
+                saldo += credito;
+                System.out.println("Saldo atualizado: R$ " + saldo);
+
+            } else if (opcao == 3) {
+                System.out.println("Quanto deseja transferir?");
+                double transferencia = sc.nextDouble();
+                    if (transferencia > saldo) {
+                       System.out.println("Saldo insuficiente!");
+                    } else {
+                       System.out.println("Transferência realizada com sucesso");
+                       saldo -= transferencia;
+                      System.out.println("Seu saldo é de: R$ " + saldo);
+                    }
+
+            } else if (opcao == 4){
+                System.out.println("Encerrando aplicação...");
+                break;
+
+            } else {
+                System.out.println("Opção inválida, informe a opção correta!");
+            }
+        }
     }
 }
